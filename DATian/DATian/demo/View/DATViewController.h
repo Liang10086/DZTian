2838f5fd022f36ded40303a24fc3480157af6076
934343686 <wangmingliang@wangmingliangdeMacBook-Pro.local>
2016-07-23 17:56:10 +0800

    已更新核心代码0.0.2
    吊炸天的代码已经更新完毕

---
+++ DATian/DATian/demo/View/DATViewController.h
@@ -0,0 +1,13 @@
+//
+//  DATViewController.h
+//  DATian
+//
+//  Created by 王明亮 on 16/7/23.
+//  Copyright © 2016年 china. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DATViewController : UIViewController
+- (void)demo;
+@end
---
+++ DATian/DATian/demo/View/DATViewController.m
@@ -0,0 +1,42 @@
+//
+//  DATViewController.m
+//  DATian
+//
+//  Created by 王明亮 on 16/7/23.
+//  Copyright © 2016年 china. All rights reserved.
+//
+
+#import "DATViewController.h"
+
+@interface DATViewController ()
+
+@end
+
+@implementation DATViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)demo{
+
+    NSLog(@"这就是传说中吊炸天的项目.你是不是已经惊呆了");
+
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
